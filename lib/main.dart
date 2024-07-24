41e061280841e87ea15294ef4e9045b2e8e28004
bojana.marojevikj <bojana@iwmnetwork.com>
2024-07-24 12:05:52 +0200

    changed font

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:classly/presentation/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,14 +17,42 @@ class MyApp extends StatelessWidget {
 
   // MyApp({required this.firestoreService});
 
+  final MaterialColor customColor = const MaterialColor(0xFF2196F3, {
+    50: Color(0xFFE3F2FD),
+    100: Color(0xFFBBDEFB),
+    200: Color(0xFF90CAF9),
+    300: Color(0xFF64B5F6),
+    400: Color(0xFF42A5F5),
+    500: Color(0xFF2196F3),
+    600: Color(0xFF1E88E5),
+    700: Color(0xFF1976D2),
+    800: Color(0xFF1565C0),
+    900: Color(0xFF0D47A1),
+  });
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Classly',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: customColor,
+          brightness: Brightness.light,
+        ),
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 50,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: GoogleFonts.poppins(
+            fontSize: 24,
+          ),
+          bodyMedium: GoogleFonts.poppins(),
+          displaySmall: GoogleFonts.poppins(),
+        ),
       ),
-      home: Splashscreen(),
+      home: const Splashscreen(),
     );
   }
 }
