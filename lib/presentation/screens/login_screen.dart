a2ec522eabd0f0d2074d8c1f99f1c5795c0523d6
elenata <elena.a@iwmnetork.com>
2024-07-26 10:09:31 +0200

    login fix

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import '../../application/services/AuthService.dart';
 import 'bottom_navigation.dart';
 import 'register_screen.dart';
@@ -10,104 +12,131 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final AuthService authService = AuthService();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _passwordVisible = false;
+  String _errorMessage = '';
+
+  Future<void> _login() async {
+    setState(() {
+      _errorMessage = '';
+    });
+
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => BottomNavigation()),
+      );
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = 'Invalid email or password';
+      });
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'An unknown error occurred';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Login'),
+        title: Text(
+          'Login',
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(color: Color(0xFF0D47A1)),
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
+              SizedBox(height: 40),
+              // Image.asset(
+              //   'assets/images/expiremind-high-resolution-logo-transparent.png',
+              //   height: 100,
+              // ),
+              // SizedBox(height: 20),
+              TextField(
                 controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
               ),
               SizedBox(height: 10),
-              TextFormField(
+              TextField(
                 controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  }
-                  return null;
-                },
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: GoogleFonts.poppins(
+                    textStyle: TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !_passwordVisible,
               ),
+              SizedBox(height: 20),
+              if (_errorMessage.isNotEmpty)
+                Text(
+                  _errorMessage,
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(color: Colors.red),
+                  ),
+                ),
               SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    loginUser(
-                      _emailController.text,
-                      _passwordController.text,
-                      context,
-                    );
-                  }
-                },
-                child: Text('Login'),
+                onPressed: _login,
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(color: Color(0xFF0D47A1)),
+                  ),
+                ),
               ),
-              SizedBox(height: 20),
               TextButton(
                 onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => RegistrationScreen()),
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => RegistrationScreen()),
                   );
                 },
-                child: Text('Don\'t have an account? Register'),
+                child: Text(
+                  'Don\'t have an account? Register',
+                  style: GoogleFonts.poppins(
+                    textStyle: TextStyle(color: Color(0xFF0D47A1)),
+                  ),
+                ),
               ),
             ],
           ),
         ),
       ),
     );
-  }
-
-  Future<void> loginUser(
-      String email,
-      String password,
-      BuildContext context,
-      ) async {
-    try {
-      await authService.login(email, password);
-
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => BottomNavigation()),
-      );
-    } catch (e) {
-      String errorMessage = 'Login failed';
-      if (e is FirebaseAuthException) {
-        final match = RegExp(r'\[(.*?)\]').firstMatch(e.message ?? '');
-        if (match != null && match.group(1) != null) {
-          errorMessage = match.group(1)!;
-        } else {
-          errorMessage = e.message ?? 'Login failed';
-        }
-      }
-
-      print('Login failed: $errorMessage');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(errorMessage)),
-      );
-    }
   }
 }
