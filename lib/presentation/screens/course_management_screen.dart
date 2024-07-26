c2b17d075509923fadea42d520e30e278204be7a
elenata <elena.a@iwmnetork.com>
2024-07-26 11:23:43 +0200

    search courses

--- lib/presentation/screens/course_management_screen.dart
+++ lib/presentation/screens/course_management_screen.dart
@@ -1,13 +1,13 @@
 import 'package:classly/application/services/CourseService.dart';
 import 'package:flutter/material.dart';
-import '../../application/services/UserService.dart';
+
 import '../../domain/models/Course.dart';
-import 'course_details_screen.dart'; // Import the course details screen
+import 'course_details_screen.dart';
 
 class CourseManagementScreen extends StatefulWidget {
   final CourseService courseService;
 
-  CourseManagementScreen({required this.courseService});
+  const CourseManagementScreen({super.key, required this.courseService});
 
   @override
   _CourseManagementScreenState createState() => _CourseManagementScreenState();
@@ -15,13 +15,23 @@ class CourseManagementScreen extends StatefulWidget {
 
 class _CourseManagementScreenState extends State<CourseManagementScreen> {
   List<Course> _courses = [];
+  List<Course> _filteredCourses = [];
   final TextEditingController _courseNameController = TextEditingController();
-  final TextEditingController _courseFullNameController = TextEditingController();
+  final TextEditingController _courseFullNameController =
+      TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _fetchCourses();
+    _searchController.addListener(_filterCourses);
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 
   void _fetchCourses() async {
@@ -29,10 +39,21 @@ class _CourseManagementScreenState extends State<CourseManagementScreen> {
       List<Course> courses = await widget.courseService.getAvailableCourses();
       setState(() {
         _courses = courses;
+        _filteredCourses = courses;
       });
     } catch (error) {
       print('Error fetching courses: $error');
     }
+  }
+
+  void _filterCourses() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredCourses = _courses.where((course) {
+        return course.courseName.toLowerCase().contains(query) ||
+            course.courseFullName.toLowerCase().contains(query);
+      }).toList();
+    });
   }
 
   void _showAddCourseDialog() {
@@ -40,35 +61,36 @@ class _CourseManagementScreenState extends State<CourseManagementScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Add New Course', style: TextStyle(color: Colors.blue),),
+          title: const Text('Add New Course', style: TextStyle(color: Colors.blue)),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               TextField(
                 controller: _courseNameController,
-                decoration: InputDecoration(labelText: 'Course Name'),
+                decoration: const InputDecoration(labelText: 'Course Name'),
               ),
-              SizedBox(height: 8),
+              const SizedBox(height: 8),
               TextField(
                 controller: _courseFullNameController,
-                decoration: InputDecoration(labelText: 'Course Full Name'),
+                decoration: const InputDecoration(labelText: 'Course Full Name'),
               ),
             ],
           ),
           actions: [
             TextButton(
-              child: Text('Cancel'),
+              child: const Text('Cancel'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: Text('Add'),
+              child: const Text('Add'),
               onPressed: () async {
                 try {
                   String courseName = _courseNameController.text;
                   String courseFullName = _courseFullNameController.text;
-                  await widget.courseService.addCourse(courseName, courseFullName);
+                  await widget.courseService
+                      .addCourse(courseName, courseFullName);
                   _courseNameController.clear();
                   _courseFullNameController.clear();
                   Navigator.of(context).pop();
@@ -88,26 +110,48 @@ class _CourseManagementScreenState extends State<CourseManagementScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Course Management'),
+        title: const Text('Course Management'),
       ),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius:
+                      BorderRadius.circular(50.0), // Make search field rounder
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  // Round the border on focus
+                  borderSide:
+                      BorderSide.none, // Remove border side for rounded corners
+                ),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+              ),
+            ),
+          ),
           ElevatedButton(
             onPressed: _showAddCourseDialog,
-            child: Text('Add New Course', style: TextStyle(color: Colors.blue),),
+            child: const Text('Add New Course', style: TextStyle(color: Colors.blue)),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: _courses.length,
+              itemCount: _filteredCourses.length,
               itemBuilder: (context, index) {
-                Course course = _courses[index];
+                Course course = _filteredCourses[index];
                 return ListTile(
                   title: Text(course.courseFullName),
                   onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => CourseDetailsScreen(course: course),
+                        builder: (context) =>
+                            CourseDetailsScreen(course: course),
                       ),
                     );
                   },
